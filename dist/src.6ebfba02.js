a7996642d91833cacceb806515ab5173ba527128
CyprienPineau <cyprien.pineau27@gmail.com>
2020-09-16 19:49:20 +0200

    dist

--- dist/src.6ebfba02.js
+++ dist/src.6ebfba02.js
@@ -28587,79 +28587,12 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./../TemplateStyleSeting.css":"src/TemplateStyleSeting.css","_css_loader":"../../../AppData/Roaming/npm-cache/_npx/17152/node_modules/parcel/src/builtins/css-loader.js"}],"../../../AppData/Roaming/npm-cache/_npx/9560/node_modules/parcel/src/builtins/bundle-url.js":[function(require,module,exports) {
-var bundleURL = null;
-
-function getBundleURLCached() {
-  if (!bundleURL) {
-    bundleURL = getBundleURL();
-  }
-
-  return bundleURL;
-}
-
-function getBundleURL() {
-  // Attempt to find the URL of the current script and use that as the base URL
-  try {
-    throw new Error();
-  } catch (err) {
-    var matches = ('' + err.stack).match(/(https?|file|ftp|chrome-extension|moz-extension):\/\/[^)\n]+/g);
-
-    if (matches) {
-      return getBaseURL(matches[0]);
-    }
-  }
-
-  return '/';
-}
-
-function getBaseURL(url) {
-  return ('' + url).replace(/^((?:https?|file|ftp|chrome-extension|moz-extension):\/\/.+)\/[^/]+$/, '$1') + '/';
-}
-
-exports.getBundleURL = getBundleURLCached;
-exports.getBaseURL = getBaseURL;
-},{}],"../../../AppData/Roaming/npm-cache/_npx/9560/node_modules/parcel/src/builtins/css-loader.js":[function(require,module,exports) {
-var bundle = require('./bundle-url');
-
-function updateLink(link) {
-  var newLink = link.cloneNode();
-
-  newLink.onload = function () {
-    link.remove();
-  };
-
-  newLink.href = link.href.split('?')[0] + '?' + Date.now();
-  link.parentNode.insertBefore(newLink, link.nextSibling);
-}
-
-var cssTimeout = null;
-
-function reloadCSS() {
-  if (cssTimeout) {
-    return;
-  }
-
-  cssTimeout = setTimeout(function () {
-    var links = document.querySelectorAll('link[rel="stylesheet"]');
-
-    for (var i = 0; i < links.length; i++) {
-      if (bundle.getBaseURL(links[i].href) === bundle.getBundleURL()) {
-        updateLink(links[i]);
-      }
-    }
-
-    cssTimeout = null;
-  }, 50);
-}
-
-module.exports = reloadCSS;
-},{"./bundle-url":"../../../AppData/Roaming/npm-cache/_npx/9560/node_modules/parcel/src/builtins/bundle-url.js"}],"src/Componants/ExperienceList.css":[function(require,module,exports) {
+},{"./../TemplateStyleSeting.css":"src/TemplateStyleSeting.css","_css_loader":"../../../AppData/Roaming/npm-cache/_npx/17152/node_modules/parcel/src/builtins/css-loader.js"}],"src/Componants/ExperienceList.css":[function(require,module,exports) {
 var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./../TemplateStyleSeting.css":"src/TemplateStyleSeting.css","_css_loader":"../../../AppData/Roaming/npm-cache/_npx/9560/node_modules/parcel/src/builtins/css-loader.js"}],"data/data_experiences.js":[function(require,module,exports) {
+},{"./../TemplateStyleSeting.css":"src/TemplateStyleSeting.css","_css_loader":"../../../AppData/Roaming/npm-cache/_npx/10568/node_modules/parcel/src/builtins/css-loader.js"}],"data/data_experiences.js":[function(require,module,exports) {
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -28935,7 +28868,7 @@ var ExperienceList = /*#__PURE__*/function (_Component2) {
 
       return /*#__PURE__*/_react.default.createElement("div", {
         id: "experiences",
-        className: "experienceList"
+        className: this.props.isSideMenuShown ? "experienceList sidemenuopen" : "experienceList"
       }, /*#__PURE__*/_react.default.createElement("h2", {
         className: "experienceList-title"
       }, "Exp\xE9riences"), ExperienceList);
@@ -29301,7 +29234,8 @@ var Content = /*#__PURE__*/function (_Component) {
       return /*#__PURE__*/_react.default.createElement("div", {
         className: "content"
       }, /*#__PURE__*/_react.default.createElement(_ExperienceList.default, {
-        showSideMenu: this.props.showSideMenu
+        showSideMenu: this.props.showSideMenu,
+        isSideMenuShown: this.props.isSideMenuShown
       }), /*#__PURE__*/_react.default.createElement(_SkillsList.default, {
         showSideMenu: this.props.showSideMenu
       }), /*#__PURE__*/_react.default.createElement("div", null, "liens externe"));
@@ -34286,7 +34220,8 @@ var Home = /*#__PURE__*/function (_Component) {
       }, /*#__PURE__*/_react.default.createElement(_Header.default, {
         isSideMenuShown: this.state.isSideMenuShown
       }), /*#__PURE__*/_react.default.createElement(_Content.default, {
-        showSideMenu: this.handleShowSideMenu
+        showSideMenu: this.handleShowSideMenu,
+        isSideMenuShown: this.state.isSideMenuShown
       }))), /*#__PURE__*/_react.default.createElement(_SideMenu.default, {
         isShawn: this.state.isSideMenuShown,
         subject: this.state.SideMenuSubject,
